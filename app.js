db0160eccf4c774495d1b97e7f6ebb87393a7d3d
Parmeet <parmeet.asi@gmail.com>
2019-04-16 15:49:48 +0530

    db connection done

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-    
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+// routes are included in this file
+const app = express();
+
+// set up body-parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// db config
+const db = require("./config/keys").mongoURI;
+
+// connect with mongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log(`server running on port ${port}`));
